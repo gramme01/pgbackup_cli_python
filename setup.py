9a981ec6ff9753950f8d1dbd999a1fb9cffbc808
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2023-08-14 23:39:43 +0000

    Implement CLI

--- setup.py
+++ setup.py
@@ -8,9 +8,16 @@ setup(
     version='0.1.0',
     author='Emmanuel Oguntoyinbo',
     author_email='gramme.oe@gmail.com',
-    description='An utility for backing up PostgreSQL databases.'
+    description='An utility for backing up PostgreSQL databases.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/gramme01/pgbackup',
-    packages=find_packages('src')
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main'
+            ],
+        }
 )
